32e5ec7451e85ce90d65f704213c84b99d4f1045
Artem <rtmbakanov@gmail.com>
2023-09-24 12:47:11 +0300

    add individual1-var3

---
+++ individ/individ-var-3.py
@@ -0,0 +1,12 @@
+# Открываем файл для чтения
+with open("file-var-3.txt", "r", encoding="utf-8") as fileptr:
+    # Читаем файл построчно
+    for line in fileptr:
+        # Используем регулярное выражение для поиска двузначных чисел
+        import re
+
+        numbers = re.findall(r'\b\d{2}\b', line)
+
+        # Если найдены двузначные числа в строке, выводим строку
+        if numbers:
+            print(line.strip())  # Убираем лишние пробелы и символы перевода строки
