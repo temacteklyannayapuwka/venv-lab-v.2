771138a4e58131e43d93eb2c33d400533f91a100
Artem <rtmbakanov@gmail.com>
2023-09-24 12:41:10 +0300

    add primer15

---
+++ primers/primer15.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+
+if __name__ == "__main__":
+    print("Number of arguments:", len(sys.argv), "arguments")
+    print("Argument List:", str(sys.argv))
+
